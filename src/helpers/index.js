e2ed9e663f6cfffd70b75be0b514cb3d2adf4b1b
kevin-sarria <81386916+kevin-sarria@users.noreply.github.com>
2022-12-03 03:02:31 -0500

    Proyecto cotizador de prestamos en vue.js Terminado

---
+++ src/helpers/index.js
@@ -0,0 +1,31 @@
+const calcularTotalPagar = (cantidad, plazo) => {
+
+    let total;
+
+    // Mientras es mator la cantidad, menor es el interes para pagar
+    if( cantidad < 5000 ) {
+        total = cantidad * 1.5;
+    } else if ( cantidad >= 5000 && cantidad < 10000 ) {
+        total = cantidad * 1.4;
+    } else if ( cantidad >= 10000 && cantidad < 15000 ) {
+        total = cantidad * 1.3;
+    } else {
+        total = cantidad * 1.2;
+    }
+
+
+    // Plazo - Más Plazo, mayor interés
+    if( plazo === 6 ) {
+        total *= 1.1;
+    } else if( plazo === 12 ) {
+        total *= 1.2;
+    } else if( plazo === 12 ) {
+        total *= 1.3;
+    }
+
+    return total;
+}
+
+export {
+    calcularTotalPagar
+}
